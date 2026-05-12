b776781d457133bded6e4c325f9564ccb8afdd3b
Xian Dev <xiandev25@gmail.com>
2026-05-13 00:16:47 +0100

    chore(app): Register the users app to the project

--- core/settings.py
+++ core/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # Local apps
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Custom User Model
+AUTH_USER_MODEL = 'users.User'
