dbd735b7c395d88da2fb36f7a0b5a16fd28becae
Xian Dev <xiandev25@gmail.com>
2026-05-13 21:20:39 +0100

    chore(app:users): Register User model in Django Admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+# Register the custom User model
+admin.site.register(User, UserAdmin)
