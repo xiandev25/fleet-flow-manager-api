f14a1147f1e7f0331c5d7db353a6b7b398a3d02d
Xian Dev <xiandev25@gmail.com>
2026-05-14 00:26:09 +0100

    chore(api:prj): Register fleet API in project URLs

--- core/urls.py
+++ core/urls.py
@@ -26,4 +26,7 @@ urlpatterns = [
     # JWT Authentication Endpoints
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    
+    # App API Endpoints
+    path('api/fleet/', include('fleet.urls')),
 ]
