dca8ab4b92952f731e6f14f50b902b5a37d2ace9
Xian Dev <xiandev25@gmail.com>
2026-05-20 07:08:13 +0100

    chore(api:prj): Load configuration settings from environment variables

--- core/settings.py
+++ core/settings.py
@@ -25,12 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+r2=pae3+!3i=bv7pedjx!+-#rh2fx1w!e&_zh+&ws0heojfbf'
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-+r2=pae3+!3i=bv7pedjx!+-#rh2fx1w!e&_zh+&ws0heojfbf')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
 
 
 # Application definition
